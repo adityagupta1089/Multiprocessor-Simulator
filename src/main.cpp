cf17f218ac2f0928420603e39422d4a7abd4e9fa
Aditya Gupta <adityagupta1089@gmail.com>
2018-03-30 16:27:03 +0530

    Minor Changes

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,20 @@ int main(int argc, char** argv) {
 	TaskGraph graph;
 	Processor processor(atoi(argv[1]), atoi(argv[2]));
 
-	printf("Reading graph\n");
+	printf("-> Reading graph\n\n");
 	read_input(argv[3], graph);
-	printf("Running the multiprocessor\n");
+	printf("-> Running the multiprocessor\n\n");
 	processor.execute_tasks(graph);
 	processor.print_statistics();
 	_time t1 = processor.get_time();
 
 	Processor serial_processor(1, 1);
 	read_input(argv[3], graph);
-	printf("Running a uniprocessor for comparison\n");
+	printf("\n-> Running a uniprocessor for comparison\n\n");
 	serial_processor.execute_tasks(graph);
 	_time t2 = serial_processor.get_time();
 
-	printf("Parallelization factor is: %f\n", t2 / (double) t1);
+	printf("-> Parallelization factor is: %f\n", t2 / (double) t1);
 
 	return 0;
 }
